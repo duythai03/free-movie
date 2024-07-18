725d6bf4cc54dfe365a9e0692bdd793e60183e59
duythai03 <21522578@gm.uit.edu.vn>
2024-07-18 17:05:23 +0700

    fix: fix route (3)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home.js";
 import MovieDetail from "./Pages/Movie/MovieDetail.js";
 import RecentMovies from "./Pages/MovieTypePages/RecentMovies.js";
@@ -9,15 +9,13 @@ import SearchResults from "./Pages/Search/SearchResults.js";
 function App() {
   return (
     <div className="">
-      <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:slug" element={<MovieDetail />} />
-          <Route path="/recent-movies" element={<RecentMovies />} />
-          <Route path="/type/:type_list" element={<OtherMovies />} />
-          <Route path="/search/:search_query" element={<SearchResults />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:slug" element={<MovieDetail />} />
+        <Route path="/recent-movies" element={<RecentMovies />} />
+        <Route path="/type/:type_list" element={<OtherMovies />} />
+        <Route path="/search/:search_query" element={<SearchResults />} />
+      </Routes>
     </div>
   );
 }
