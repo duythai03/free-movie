e43552d8db72b2bf4bcb17159c4778c4dc16ba72
duythai03 <148519165+duythai03@users.noreply.github.com>
2025-08-11 13:02:47 +0900

    Enhance search function

--- src/Components/SearchInput.js
+++ src/Components/SearchInput.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import normalize from "normalize-text";
 import { ThemeContext } from "../Context/ThemeContext";
 import axios from "axios";
 
@@ -51,11 +50,10 @@ function SearchInput() {
       console.log("Keyword is empty");
       return;
     }
-    const normalizedKeyword = normalize(keyword);
     setIsSearchVisible(false);
     setSuggestions([]);
     setKeyword("");
-    navigate(`/search/${encodeURIComponent(normalizedKeyword)}`);
+    navigate(`/search/${keyword.trim()}`);
   };
 
   const handleKeyDown = (e) => {
@@ -81,20 +79,46 @@ function SearchInput() {
 
       {isSearchVisible && (
         <div className="fixed top-[80px] left-0 w-screen p-1 z-20 bg-white shadow-md">
-          <input
-            type="text"
-            placeholder="Tìm kiếm phim..."
-            className="w-full p-2 pl-4 rounded-full text-black bg-white border border-gray-300 focus:outline-none"
-            value={keyword}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
-          />
-          <i
-            className="absolute right-4 top-4 text-lg text-gray-500 cursor-pointer"
-            onClick={handleSearch}
-          >
-            <FaMagnifyingGlass />
-          </i>
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Tìm kiếm phim..."
+              className="w-full p-2 pl-4 rounded-full text-black bg-white border border-gray-300 focus:outline-none"
+              value={keyword}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+            <i
+              className="absolute right-4 top-2 text-lg text-gray-500 cursor-pointer"
+              onClick={handleSearch}
+            >
+              <FaMagnifyingGlass />
+            </i>
+            {suggestions.length > 0 && keyword.length > 0 && (
+              <div className="absolute z-10 bg-black bg-opacity-70 rounded-md shadow-md top-[100%] left-0 w-full min-w-[200px] min-h-[70px]">
+                {suggestions.map((item) => (
+                  <div
+                    key={item.slug}
+                    className="flex items-center p-2 hover:bg-gray-700"
+                  >
+                    <div className="w-10 h-10 rounded-lg overflow-hidden flex-shrink-0 mr-2">
+                      <img
+                        src={`https://phimimg.com/${item.poster_url}`}
+                        alt={item.name}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                    <div
+                      className="text-white text-xs cursor-pointer"
+                      onClick={() => handleSuggestionClick(item.slug)}
+                    >
+                      {item.name}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       )}
 
@@ -114,7 +138,7 @@ function SearchInput() {
         >
           <FaMagnifyingGlass />
         </i>
-        {suggestions.length > 0 && (
+        {suggestions.length > 0 && keyword.length > 0 && (
           <div className="absolute z-10 bg-black bg-opacity-70 rounded-md shadow-md top-[100%] left-0 w-full min-w-[200px] min-h-[70px]">
             {suggestions.map((item) => (
               <div
